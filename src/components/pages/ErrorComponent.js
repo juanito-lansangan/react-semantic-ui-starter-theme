8a6a59e3b3e10f04b919ff3aa3e4113a0a09b987
jplans <juanito.lansangan@gmail.com>
2018-07-21 20:48:58 +0800

    remove unused imports

--- src/components/pages/ErrorComponent.js
+++ src/components/pages/ErrorComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Message, Button, Icon } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom';
 
 const ErrorComponent = (props) => {
